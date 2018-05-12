dfbc8b11813b05ecd997015c67a5d3322c110cb9
Takeshi Shimmura <takeshi@Takeshi-no-MacBook-Air.local>
2018-05-13 00:10:15 +0900

    Corrplot and Decison Tree graph

--- Decision_Tree.R
+++ Decision_Tree.R
@@ -1,21 +1,31 @@
 
 #データのインポート
 raw_data <- read.csv('census_new.csv')
-
-#相関を確認
-cor(raw_data)
-
 #サマリー
 summary(raw_data)
+
+
+#
+# 1. 相を確認
+#
+
+R <- cor(raw_data)
+library(corrplot)
+corrplot(R, method = 'circle')
+corrplot(R, method = 'shade')
+#corrplot(R, method = 'number')
+#https://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
+
+
+#
+# 2. 決定木
+#
+
 #ライブラリを読み込む
 library('rpart')
-
 #データを抽出
 d1.rp<-rpart(income~.,raw_data)
-
 #決定木を描写
 plot(d1.rp,uniform=T,margin=0.2)
-
 #テキストを付与
 text(d1.rp,uniform=T,use.n=T,all=F)
-
